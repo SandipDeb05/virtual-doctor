d3e2d82ac28e8229f6217f52942925384db143c4
Sandip Deb <1707044@kiit.ac.in>
2020-06-12 12:51:49 +0530

    Updates

---
+++ precache-manifest.5c2cc32386d2890c8e5ff06f0146e5ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71df43a488af307bb9c752be7533b520",
+    "url": "/virtual-doctor/index.html"
+  },
+  {
+    "revision": "ac0452e3112a94c6b201",
+    "url": "/virtual-doctor/static/css/main.c4b7ec41.chunk.css"
+  },
+  {
+    "revision": "d106b866aad20f70745e",
+    "url": "/virtual-doctor/static/js/2.f5d87487.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/virtual-doctor/static/js/2.f5d87487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac0452e3112a94c6b201",
+    "url": "/virtual-doctor/static/js/main.7a220b25.chunk.js"
+  },
+  {
+    "revision": "3aab41e08cfefaf5aa7e",
+    "url": "/virtual-doctor/static/js/runtime-main.6c39227a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/virtual-doctor/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8b76ebf906d93b0fee4f554c5431c5c7",
+    "url": "/virtual-doctor/static/media/logo192.8b76ebf9.png"
+  }
+]);
